74f3c5c8dea49d460ce65cc6ee6c4ae84cd0d109
MiiKY25 <mikel.ra.2003@gmail.com>
2025-05-07 20:32:37 +0200

    add:ventana inicio

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module org.hospedix {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
+    opens org.hospedix.controladores to javafx.fxml;
     opens org.hospedix to javafx.fxml;
     exports org.hospedix;
 }
---
+++ src/main/java/org/hospedix/controladores/ControllerInicio.java
@@ -0,0 +1,17 @@
+package org.hospedix.controladores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ControllerInicio {
+
+    @FXML
+    private TextField txtDNI;
+
+    @FXML
+    void accionIniciarSesion(ActionEvent event) {
+
+    }
+
+}
